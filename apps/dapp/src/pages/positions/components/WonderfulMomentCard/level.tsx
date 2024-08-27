ceaaa0548da93d83528bbe9b5eb3191208896459
Justyn <justyn@sofa.org>
2024-08-27 19:57:22 +0800

    feat: icon of wonderful icon

--- apps/dapp/src/pages/positions/components/WonderfulMomentCard/level.tsx
+++ apps/dapp/src/pages/positions/components/WonderfulMomentCard/level.tsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 import { Modal } from '@douyinfe/semi-ui';
 import { useTranslation } from '@sofa/services/i18n';
 
 import { addI18nResources } from '@/locales';
 
+import img1 from './assets/1.png';
+import img2 from './assets/2.png';
+import img3 from './assets/3.png';
+import img4 from './assets/4.png';
+import img5 from './assets/5.png';
+import img6 from './assets/6.png';
+import img7 from './assets/7.png';
+import img8 from './assets/8.png';
 import { Comp as IconInfo } from './assets/icon-info.svg';
 import ruleDescriptions from './assets/rule-descriptions.png';
 import locale from './locale';
@@ -13,47 +21,47 @@ import styles from './index.module.scss';
 addI18nResources(locale, 'RuleDescriptions');
 
 export const WonderfulMomentLevels: {
-  icon: string;
+  icon: ReactNode;
   color: string;
   is(returnInUsd: number): boolean;
 }[] = [
   {
-    icon: '🚀',
+    icon: <img style={{ width: 64 }} src={img8} alt="" />,
     color: 'linear-gradient(135deg, #D1B32F, #FFCF00)',
     is: (returnInUsd) => returnInUsd >= 10000,
   },
   {
-    icon: '⛵️',
+    icon: <img style={{ width: 64 }} src={img7} alt="" />,
     color: 'linear-gradient(135deg, #00FFFF, #0F477A)',
     is: (returnInUsd) => returnInUsd >= 7500,
   },
   {
-    icon: '💐',
+    icon: <img style={{ width: 64 }} src={img6} alt="" />,
     color: 'linear-gradient(135deg,  #D1B32F, #FFCF00)',
     is: (returnInUsd) => returnInUsd >= 5000,
   },
   {
-    icon: '🎁',
+    icon: <img style={{ width: 64 }} src={img5} alt="" />,
     color: 'linear-gradient(135deg, #00FFFF, #0F477A)',
     is: (returnInUsd) => returnInUsd >= 2500,
   },
   {
-    icon: '💰',
+    icon: <img style={{ width: 64 }} src={img4} alt="" />,
     color: 'linear-gradient(135deg,  #D1B32F, #FFCF00)',
     is: (returnInUsd) => returnInUsd >= 1000,
   },
   {
-    icon: '🍺',
+    icon: <img style={{ width: 64 }} src={img3} alt="" />,
     color: 'linear-gradient(135deg, #00FFFF, #0F477A)',
     is: (returnInUsd) => returnInUsd >= 500,
   },
   {
-    icon: '🍬',
+    icon: <img style={{ width: 64 }} src={img2} alt="" />,
     color: 'linear-gradient(135deg,  #D1B32F, #FFCF00)',
     is: (returnInUsd) => returnInUsd >= 100,
   },
   {
-    icon: '✨',
+    icon: <img style={{ width: 64 }} src={img1} alt="" />,
     color: 'linear-gradient(135deg, #00FFFF, #0F477A)',
     is: (returnInUsd) => returnInUsd >= 0,
   },
