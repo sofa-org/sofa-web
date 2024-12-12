9943b5823dd4fff7eab1764a2c5a179ec7d81e43
Justyn <justyn@sofa.org>
2024-12-12 13:04:06 +0800

    feat(Header): hover

--- apps/dapp/src/components/Header/index-home.tsx
+++ apps/dapp/src/components/Header/index-home.tsx
@@ -166,11 +166,9 @@ export const RenderMenu = (it: MenuItem) => {
   );
   return (
     <Dropdown
-      // trigger={Env.isMobile || Env.isTelegram ? 'click' : 'hover'}
-      trigger={'click'}
+      trigger={Env.isMobile || Env.isTelegram ? 'click' : 'hover'}
       className={classNames(styles['nav-selector'], 'semi-always-dark')}
       position={'bottomLeft'}
-      // keepDOM
       render={
         <Dropdown.Menu className={styles['nav-selector-items']}>
           {Object.entries(groups).map(([group, children], _, arr) => {
