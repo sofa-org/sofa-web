724621308d20ea2530d7b77cb61927d634ebcbcc
dashi <dashi.xiao@sofa.org>
2025-04-01 14:50:45 +1100

    fix(dual): ui

--- apps/dapp/src/pages/products/components/FlowDual/index.tsx
+++ apps/dapp/src/pages/products/components/FlowDual/index.tsx
@@ -2,16 +2,12 @@ import { useTranslation } from '@sofa/services/i18n';
 import { useLocalStorageState } from 'ahooks';
 import classNames from 'classnames';
 
-import { Comp as Step1 } from './assets/1.svg';
-import { Comp as Step2 } from './assets/2.svg';
-import { Comp as Step3 } from './assets/3.svg';
-import { Comp as Step4 } from './assets/4.svg';
 import { Comp as IconExpand } from './assets/icon-expand.svg';
 
 import styles from './index.module.scss';
 
 export const FlowDual = () => {
-  const [t] = useTranslation();
+  const [t, i18n] = useTranslation();
   const [expanded, setExpanded] = useLocalStorageState<'1' | '0'>(
     'product-flow',
     { defaultValue: '1' },
@@ -20,6 +16,7 @@ export const FlowDual = () => {
     <div
       className={classNames(styles['flow-wrapper'], {
         [styles['expanded']]: expanded === '1',
+        [styles['lang-zh']]: /^zh/i.test(i18n.language),
       })}
       onClick={() => setExpanded((pre) => (pre === '1' ? '0' : '1'))}
     >
@@ -27,13 +24,13 @@ export const FlowDual = () => {
         {t({ enUS: 'Getting Started with Dual' })}
       </div>
       <div className={styles['content']}>
-        <Step1 width={163 / window.winScale} />
+        <div className={styles['step1']} />
         <div className={styles['arrow']} />
-        <Step2 width={108 / window.winScale} />
+        <div className={styles['step2']} />
         <div className={styles['arrow2']} />
-        <Step3 width={394 / window.winScale} />
+        <div className={styles['step3']} />
         <div className={styles['arrow3']} />
-        <Step4 width={138 / window.winScale} />
+        <div className={styles['step4']} />
       </div>
       <IconExpand className={styles['icon-expand']} />
     </div>
