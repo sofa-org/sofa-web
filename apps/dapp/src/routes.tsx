f1de1c56789461d119343671b1b087b7d4fe4187
ytSOFA <tuo.yang@sofa.org>
2025-10-14 17:21:08 +0800

    feat(seo): fill title, description and keywords content

--- apps/dapp/src/routes.tsx
+++ apps/dapp/src/routes.tsx
@@ -39,76 +39,115 @@ export const routes: {
   {
     path: '/',
     Component: Home,
+    title: 'SOFA.org',
+    description:
+      "SOFA.org is a decentralized, nonprofit, open-source technology organization offering protocols for crypto products, especially options. Earn $RCH via protocol use, liquidity provision, and governance. $SOFA enables holders to vote on proposals that shape SOFA.org's future.",
+    keywords:
+      'DeFi, RCH, SOFA, decentralized finance, crypto structured products, blockchain, protocol safety, community rewards, financial technology, governance token, decentralized organization, DeFi education, capital efficiency, nonprofit finance',
   },
   {
-    path: '/test', // 开发
+    path: '/test',
     Component: Test,
   },
   {
     path: '/policy',
     Component: Policy,
+    title: 'Policy - SOFA.org',
   },
   {
     path: '/mechanism',
     Component: Mechanism,
+    title: 'Project - SOFA.org',
+    description: '100% on-chain and tokenized positions.',
   },
   {
     path: '/strengths',
     Component: Advantages,
+    title: 'Capabilities - SOFA.org',
+    description:
+      'The SOFA protocol is our ambitious attempt to establish standards of how financial assets can be atomically settled on-chain, while simultaneously catalyzing DeFi capital liquidity through transferrable Position Tokens.',
   },
   {
     path: '/rch',
     Component: RCH,
-    title: 'RCH',
-    description: '这是 RCH 的页面描述',
-    keywords: 'RCH, RCH',
+    title: 'RCH - SOFA.org',
+    description:
+      'Earn $RCH via protocol use, liquidity provision, and governance.',
+    keywords: 'RCH, airdrop',
   },
   {
     path: '/points',
     Component: Points,
+    title: 'Points - SOFA.org',
+    description:
+      'Discover the rules for earning $SOFA points on the SOFA protocol. See how interacting with our DeFi products and options qualifies you for $SOFA airdrop.',
+    keywords: 'SOFA points, airdrop',
   },
   {
     path: '/positions',
     Component: Positions,
+    title: 'Positions - SOFA.org',
   },
   {
     path: '/products',
     Component: Products,
+    title: 'Products - SOFA.org',
+    description:
+      'Trade options products like Earn, Surge, and Dual, and follow Automator strategies on our protocol to get $RCH airdrops.',
+    keywords: 'earn, surge, dual, airdrop',
   },
   {
     path: '/products/customize',
     Component: ProductCustomize,
+    title: 'Customize - SOFA.org',
+    description:
+      'Customize DeFi options products tailored to your investment strategy and risk preferences on SOFA platform and get exclusive $RCH airdrops.',
   },
   {
     path: '/products/automator',
     Component: Automator,
+    title: 'Automator - SOFA.org',
+    description:
+      'Automator is a DeFi product to follow top strategies or create your own to earn profits and receive exclusive $RCH airdrops.',
+    keywords: 'automator, airdrop',
   },
   {
     path: '/products/automator/mine',
     Component: AutomatorMine,
+    title: 'My Automators - SOFA.org',
   },
   {
     path: '/products/automator/operate',
     Component: AutomatorOperate,
+    title: 'Operate Automator - SOFA.org',
   },
   {
     path: '/positions/orders',
     Component: OrderHistory,
+    title: 'Order History - SOFA.org',
   },
   {
     path: '/automator/positions',
     Component: AutomatorPositions,
+    title: 'Automator Positions - SOFA.org',
   },
   {
     path: '/products/automator/create',
     Component: AutomatorCreate,
+    title: 'Create Automator - SOFA.org',
+    description: 'Create your own automator and roll it to earn profits.',
   },
   {
     path: '/transactions',
     Component: OrderHistory,
+    title: 'Transactions History - SOFA.org',
   },
   {
     path: '/a',
     Component: AutomatorSharePage,
+    title: 'Automator Sharing - SOFA.org',
+    description:
+      'Follow Automator strategies on our protocol to earn profits and get exclusive $RCH airdrops.',
+    keywords: 'automator, airdrop',
   },
 ];
