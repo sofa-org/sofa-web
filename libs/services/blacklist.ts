dd1e3d9f2ce895b2ef26bcb52d4a8e197be4b787
Justyn <justyn@sofa.org>
2025-11-16 09:59:06 +0800

    feat: add blacklist

--- libs/services/blacklist.ts
+++ libs/services/blacklist.ts
@@ -1,7 +1,10 @@
 import { Env } from '@sofa/utils/env';
 
 export class BlacklistService {
-  static blacklist = ['0x6d6a3a7f560b533626c4d618d748b087ef87feea'];
+  static blacklist = [
+    '0x6d6a3a7f560b533626c4d618d748b087ef87feea',
+    '0xc21c0885b22EAF7594670327B0aFC5F6efE026b4', // 黑客地址
+  ];
 
   static shouldBlock(wallet?: string) {
     if (!wallet) return false;
